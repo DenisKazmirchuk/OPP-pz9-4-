282c2f5a7b9aee2e02bd25babcb6c8fe16567ca6
DenisKazmirchuk <161360416+DenisKazmirchuk@users.noreply.github.com>
2024-12-02 20:12:53 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Func<int, long> doubleFactorial = n =>
+        {
+            long factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            return 2 * factorial;
+        };
+
+        Console.Write("Введіть число n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"2 * {n}! = {doubleFactorial(n)}");
+    }
+}
